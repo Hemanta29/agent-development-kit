d621639ca657c732106c34e246b5974cd7813abc
Hem Dutta <hemantadutta29@gmail.com>
2025-07-21 18:14:19 +0530

    Basic Agent Done

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -2,12 +2,10 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
     name="greeting_agent",
-    description="An agent that greets the user and provides information about the current time.",
-    model="google/gemini-2.5-pro",
-    instructions="""
-You are a helpful agent that greets the user and provides information about the current time.
-When the user asks for the current time, you should respond with the current time in the format.
-'The current time is HH:MM AM/PM'.
-If the user asks for a greeting, you should respond with a friendly greeting.
+    description="An agent that greets the user.",
+    model="gemini-2.5-pro",
+    instruction="""
+You are a helpful agent that greets the user.
+Ask for the user's name and greet them by their name.
 """,
 )
